295931a3449ba0a086d0e4837cf84ca5c0a8ee70
jekabsj <50320205+jekabsj@users.noreply.github.com>
2023-11-26 19:32:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ function generatePostcard() {
     }
 
     var postcardImage = document.getElementById('postcardImage');
+    var postcardText = document.getElementById('postcardText');
     var downloadLink = document.getElementById('downloadLink');
 
     // Izveido attēlu ar dinamisko tekstu
     var imageUrl = 'https://via.placeholder.com/600x400.png?text=Skola,+kur%C4%81+katrs+' + encodeURIComponent(message) + '+ir+v%C4%93rt%C4%ABba';
     postcardImage.src = imageUrl;
     postcardImage.style.display = 'block';
+
+    // Uzstāda dinamisko tekstu uz pastkartes
+    postcardText.innerText = 'Skola, kurā katrs ' + message + ' ir vērtība';
 
     // Parāda lejupielādes saiti
     downloadLink.href = imageUrl;
